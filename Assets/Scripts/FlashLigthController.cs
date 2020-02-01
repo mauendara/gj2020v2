34bfbe6af66e885703df31bfe6f65fa8ebcd1faf
aicalvopina <kyubi900@hotmail.com>
2020-02-01 09:07:11 -0500

    flashligth controller

---
+++ Assets/Scripts/FlashLigthController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLigthController : MonoBehaviour
+{
+    public Light ligth;
+    private Vector2 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 move = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (move.x < 0) 
+		{
+            ligth.transform.eulerAngles = new Vector3 (0, -90, 0);
+        }
+        else if (move.x > 0)
+        {
+            ligth.transform.eulerAngles = new Vector3 (0, 90, 0);
+        } 
+    }
+}
