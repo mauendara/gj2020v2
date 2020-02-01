6f767c71973a019768bdc6596d9e4bcdaa6a95de
aicalvopina <kyubi900@hotmail.com>
2020-02-01 12:13:37 -0500

    player con linterna

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,8 +14,8 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position-offset;
+        transform.position = player.transform.position + offset;
     }
 }
