64a8869ac6bea11e67a8f56402e74bd5761ed884
Mauricio Endara <mauricio.endara@alterbios.com>
2020-02-01 13:43:46 -0500

    merge

---
+++ Assets/Scripts/P_enemyMoves.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_enemyMoves : MonoBehaviour {
+
+    private Vector3 initialPosition;
+    public float maxDist = 5;
+    private int direction;
+    public float movingSpeed= 5;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb2d=GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+        direction = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (direction == 1)
+        {
+            if(transform.position.x - initialPosition.x > maxDist){
+                spriteRenderer.flipX = true;
+                direction = -1;
+            }
+
+        }
+        else
+        {
+            if (transform.position.x < initialPosition.x )
+            {
+                spriteRenderer.flipX = false;
+                direction = 1;
+            }
+        }
+        rb2d.velocity = new Vector2(direction*movingSpeed, rb2d.velocity.y);
+
+	}
+}
---
+++ Assets/Scripts/PickUpItemScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform player;
+    public float throwForce = 10;
+    private bool isPlayerOver = false;
+    private bool isBeingCarried = false;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        isPlayerOver = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        isPlayerOver = false;
+    }
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingCarried)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Rigidbody2D>().isKinematic = false;
+                transform.parent = null;
+                isBeingCarried = false;
+                GetComponent<Rigidbody2D>().AddForce(player.forward * throwForce);
+                Debug.Log("Droping");
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E) && isPlayerOver)
+            {
+                GetComponent<Rigidbody2D>().isKinematic = true;
+                transform.parent = player;
+                isBeingCarried = true;
+                Debug.Log("Picking up");
+            }
+        }
+    }
+}
