a9e2907ec9713eb25ff60b1f932dc4f6ae1eb846
Mauricio Endara <mauricio.endara@alterbios.com>
2020-02-01 13:40:10 -0500

    Uno y Dos scenes constructed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,15 +7,16 @@ public class CameraController : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+    
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;    
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x-offset.x,transform.position.y,transform.position.z);
+        transform.position = new Vector3(player.transform.position.x - offset.x, transform.position.y, transform.position.z);
     }
 }
